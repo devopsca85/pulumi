c908b9f1f8a375a918c5d1d61ac419230284f44d
devopsca85 <102137949+devopsca85@users.noreply.github.com>
2023-09-04 14:38:27 -0400

    Updated by Isingh

--- index.ts
+++ index.ts
@@ -1,64 +1,67 @@
-import * as azure from "@pulumi/azure";
 import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources";
-import * as network from "@pulumi/azure-native/network";
-import * as containerservice from "@pulumi/azure-native/containerservice";
-import * as compute from "@pulumi/azure-native/compute";
+import * as azure from "@pulumi/azure";
 import * as azure_native from "@pulumi/azure-native";
 
-// Config and defaults
-const location = "EastUS2";
+const resourceGroup = new azure.core.ResourceGroup("resourceGroup");
 
-// Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup("demo-rg", {
-    location: location,
-    name: "demo-rg",
-});
-
-// Create an Azure Virtual Network
-const vnet = new azure.network.VirtualNetwork("demo2023", {
-    location: resourceGroup.location,
+const frontdoorExample = new azure_native.network.FrontDoor("example", {
     resourceGroupName: resourceGroup.name,
-    addressSpaces: ["10.0.0.0/16"],
-    name: "demo2023",
+    location: "global",
+    enforceBackendPoolsCertificateNameCheck: false,
+    frontendEndpoints: [{
+        name: "frontend-endpoint1",
+        hostName: "examplefrontdoor50c0a30.azurefd.net",
+        //sessionAffinityEnabled: false,
+       // sessionAffinityTtlSeconds: 0,
+      //  webApplicationFirewallPolicyLink: {
+      //      id: "some-id",
+      //  },
+    }],
+    backendPools: [{
+        name: "example-backendpool",
+        backends: [{
+            address: "example.com",
+            enabledState: "Enabled",
+            httpPort: 80,
+            httpsPort: 443,
+            priority: 1,
+            weight: 50
+        }],
+        healthProbeSettings: {
+            name: "example-healthprobe",
+        },
+        loadBalancingSettings: {
+            name: "example-loadbalancingsettings",
+        },
+    }],
+    healthProbeSettings: [{
+        name: "example-healthprobe",
+        enabledState: "Enabled",
+        path: "/",
+        probeMethod: "HEAD",
+        protocol: "Http",
+    }],
+    loadBalancingSettings: [{
+        name: "example-loadbalancingsettings",
+        additionalLatencyMilliseconds: 0,
+        sampleSize: 4,
+        successfulSamplesRequired: 2
+    }],
+    routingRules: [{
+        name: "example-routingrule",
+        acceptedProtocols: ["Https"],
+        enabledState: "Enabled",
+        frontendEndpoints: ["frontend-endpoint1"],
+        patternsToMatch: ["/*"],
+        routeConfiguration: {
+            forwardingConfiguration: {
+                backendPoolName: "example-backendpool",
+                customForwardingPath: "/",
+                forwardingProtocol: "HttpsOnly",
+                cacheUseDynamicCompression: false
+            },
+        }
+    }],
 });
 
-// Create an Azure KeyVault
-const keyVault = new azure.keyvault.KeyVault("demo-kv", {
-    location: resourceGroup.location,
-    resourceGroupName: resourceGroup.name,
-    skuName: "standard",
-    tenantId: "<YOUR_AZURE_AD_TENANT_ID>",
-    name: "demo-kv",
-});
-
-// Create an Azure App Configuration
-const appConfig = new azure.appconfiguration.ConfigurationStore("demo-cfg", {
-    location: resourceGroup.location,
-    resourceGroupName: resourceGroup.name,
-    sku: "standard",
-    name: "demo-cfg",
-});
-
-// Create a private Azure Kubernetes Cluster
-const kubernetesCluster = new azure.containerservice.KubernetesCluster("gwr-demo-aks", {
-    location: resourceGroup.location,
-    resourceGroupName: resourceGroup.name,
-    defaultNodePool: {
-        name: "default",
-        nodeCount: 1,
-        vmSize: "Standard_D2_v2",
-    },
-    dnsPrefix: "aks",
-    privateClusterEnabled: true,
-    name: "gwr-demo-aks",
-});
-
-// Create Azure Front Door Premium
-//const frontDoor = new azure.network.frontDoor("gwrdemoaks", {
-  //  resourceGroupName: resourceGroup.name,
-   // location: resourceGroup.location,
-    // ... add more configurations as necessary
-//});
-
-export const resourceGroupName = resourceGroup.name;
+export const frontDoorUrl = frontdoorExample.defaultHostname;
